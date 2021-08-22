aba1ea87a85933fe7412ff2ce80adf8b7b27765e
Pedro Helias <pedrohelias95@hotmail.com>
2021-08-22 17:13:17 -0300

    adicionado botão e useState

--- curso_dicas_para_devs/src/App.js
+++ curso_dicas_para_devs/src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 
+
 function App(){
+  // let mensagem = "uma mensagem aqui!"
+  const [mensagem, setMensagem] = useState(''); //o UseState me da o estado atual da variavel
 
-  return <div className="container">
-    <h1>EU AQUI</h1>
-  </div>;
+  function alteramsg(){
+    
+    setMensagem("nova mensagem");
+  }
 
+  return (
+    <div>
+      
+      <div className="container">
+        <h1>{mensagem}</h1>
+      </div>;
+      
+      <button onClick={alteramsg}>
+        mudar mensagem
+      </button>;
+
+    </div>
+  );
 };
 
 
