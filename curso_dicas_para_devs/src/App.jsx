f608b40810b0b9292a8f04f1b9905f89faef218c
Pedro Helias <pedrohelias95@hotmail.com>
2021-09-03 17:08:48 -0300

    adições finais e finalização do projeto

--- curso_dicas_para_devs/src/App.jsx
+++ curso_dicas_para_devs/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import AddTask from "./components/AddTask";
 import Button from "./components/Button";
@@ -7,6 +7,7 @@ import {v4 as uuidv4} from "uuid";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import TaskDetails from "./components/TaskDetails";
+import axios from "axios"; //para fazer requisição http para a API, é necessário fazer a requisição logo quando a página é montada
 
 const App = () => {
   //let mensagem = "olá mundo!";
@@ -29,7 +30,24 @@ const App = () => {
 
   
 ]); //ao utilizar um state, sempre se coloca[variavel a ser alterada, nome da função que vai altera-la]
-  const handleTaskClickmodified = (taskId) => {
+ 
+  useEffect(()=>{
+    console.log("foi renderizado pela primeira vez")
+
+   const fetchTasks = async() => {
+
+      const {data} = await axios.get("https://jsonplaceholder.cypress.io/todos?_limit-10") //pegando tarefas da api
+
+      setTasks(data) //setando na aplicação
+    };
+   
+    
+    //useEffect é feito para não retornar nada
+    fetchTasks();
+  }, []); //useEffect é um hook. Ele executa esse bloco de código sempre que uma variável dentro do useEffect muda
+//para executar sempre que a tela inicia, é só deixar a lista vazia
+
+const handleTaskClickmodified = (taskId) => {
     const newTasks = tasks.map((task)=>{
       if(task.id === taskId) return {...task, completed: !task.completed}
       return task;
